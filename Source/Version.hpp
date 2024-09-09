b6c6aa883c308c0870469f1fce5f6869eba8ca0f
isaiah <isaiahjclark21@gmail.com>
2024-09-08 18:16:49 -0600

    build

--- Source/Version.hpp
+++ Source/Version.hpp
@@ -21,13 +21,13 @@ along with Lugaru.  If not, see <http://www.gnu.org/licenses/>.
 /** Version number is either MAJOR.MINOR, or MAJOR.MINOR.PATCH if PATCH != 0 */
 const unsigned int VERSION_MAJOR = 1;
 const unsigned int VERSION_MINOR = 2;
-const unsigned int VERSION_PATCH = 3;
-const std::string VERSION_NUMBER = "1.2.3";
+const unsigned int VERSION_PATCH = 1;
+const std::string VERSION_NUMBER = "1.2.1";
 
 /** Set to "" for stable (tagged) builds, "-dev" for dev builds */
 const std::string VERSION_SUFFIX = "-dev";
 /** Set to 7-char git commit hash if available, otherwise "" */
-const std::string VERSION_HASH = "ee0d1ff";
+const std::string VERSION_HASH = "937e104";
 /** Optional release string, e.g. for distro packages release number */
 const std::string VERSION_RELEASE = "";
 
@@ -40,7 +40,7 @@ const std::string VERSION_RELEASE = "";
  *      "1.2-dev (git ab12c34) [OSS Lugaru official]"
  *      "1.3.1 [Mageia 1.3.1-2.mga6]"
  */
-const std::string VERSION_STRING = "1.2.3-dev (git ee0d1ff)";
+const std::string VERSION_STRING = "1.2.1-dev (git 937e104)";
 
 /** Build type (Release, Debug, RelWithDebInfo) to output to the terminal */
 const std::string VERSION_BUILD_TYPE = "";
